e78f12faceeb3b1ad367fb1c193ad2f4caef3d13
Lauren <lisle.lauren@gmail.com>
2020-03-02 21:15:24 -0500

    working on appspt!

--- lib/db2.py
+++ lib/db2.py
@@ -1,38 +1,55 @@
 import os
 import psycopg2
+from psycopg2.pool import SimpleConnectionPool
+from contextlib import contextmanager
 
 db_user = os.environ.get("DB_USER")
 db_pass = os.environ.get("DB_PASS")
 db_name = os.environ.get("DB_NAME")
 cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
+host = "/cloudsql/" + cloud_sql_connection_name
 
-conn = psycopg2.connect(database=db_name, user = db_user, password = db_pass, host = "/cloudsql/" + cloud_sql_connection_name)
-curr = conn.cursor()
-curr.execute(
-        "CREATE TABLE IF NOT EXISTS data "
-        "( athlete_id TEXT, unfin TEXT, finished TEXT, MTS TEXT, polylines TEXT, PRIMARY KEY (athlete_id) );"
-    )
+#conn = psycopg2.connect(database=db_name, user = db_user, password = db_pass, host = "/cloudsql/" + cloud_sql_connection_name)
+dbConnection = "dbname=" + db_name + ' user=' + db_user + ' host=' + host + ' password=' + db_pass
+connectionpool = SimpleConnectionPool(1, 10, dsn=dbConnection)
+
+
+@contextmanager
+def getcursor():
+    con = connectionpool.getconn()
+    try:
+        yield con.cursor()
+        con.commit()
+    finally:
+        connectionpool.putconn(con)
+
+
+with getcursor() as cur:
+    cur.execute(
+            "CREATE TABLE IF NOT EXISTS data "
+            "( athlete_id TEXT, unfin TEXT, finished TEXT, MTS TEXT, polylines TEXT, PRIMARY KEY (athlete_id) );"
+        )
 
 # Make a convenience function for running SQL queries
 def sql_query(query):
-    cur = conn.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
+    with getcursor() as cur:
+        cur.execute(query)
+        rows = cur.fetchall()
     return rows
 
 def sql_edit_insert(query,var):
-    cur = conn.cursor()
-    cur.execute(query,var)
-    conn.commit()
+    with getcursor() as cur:
+        cur.execute(query,var)
+
 
 def sql_delete(query,var):
-    cur = conn.cursor()
-    cur.execute(query,var)
+    with getcursor() as cur:
+        cur.execute(query,var)
 
 def sql_query2(query,var):
-    cur = conn.cursor()
-    cur.execute(query,var)
-    rows = cur.fetchall()
+    with getcursor() as cur:
+        cur.execute(query,var)
+        rows = cur.fetchall()
     return rows
 
 
--- main.py
+++ main.py
@@ -274,14 +274,19 @@ def get_username(access_token, MTS):
     MTS = json.dumps(MTS)
     polylines = json.dumps(polylines)
 
+    print('before athlete id', athlete_id)
+    print('before unfin', unfin)
+    print('before finsihed', finished)
+    print('before mts', MTS)
+    print('before polylines', polylines)
+
     #check if athlete exists
     db_athletes = [r[0] for r in db2.sql_query('''SELECT athlete_id FROM data''')]
     if athlete_id in db_athletes:
-        db2.sql_edit_insert('''UPDATE data SET unfin = %s, fin = %s, mts = %s, polylines = %s WHERE athlete_id = %s''', (unfin, finished, MTS, polylines, athlete_id)
+        db2.sql_edit_insert('''UPDATE data SET unfin = %s, finished = %s, mts = %s, polylines = %s WHERE athlete_id = %s''', (unfin, finished, MTS, polylines, athlete_id)
 )
     else:
         db2.sql_edit_insert('''INSERT INTO data (athlete_id, unfin, finished, mts, polylines) VALUES (%s, %s, %s, %s, %s) ''',(athlete_id, unfin, finished, MTS, polylines))
-    db2.conn.commit()
 
 
 
